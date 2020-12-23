47522aed1a5c4a08c2d6013feed8ea5963086cf1
Pablo Olmedo <pablo.olmedo@cba.gob.ar>
2020-12-23 11:36:08 +1000

    Updates

---
+++ precache-manifest.067dcc4f2958b2be057773c2a26fa491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e499aea7bb1300ee6acd9d8ba4150b66",
+    "url": "/Budget_App_ReactJS/index.html"
+  },
+  {
+    "revision": "eb8f5b7c40b5f5e30e71",
+    "url": "/Budget_App_ReactJS/static/css/main.f4451901.chunk.css"
+  },
+  {
+    "revision": "35ff1e67a448da6e77b4",
+    "url": "/Budget_App_ReactJS/static/js/2.ce226546.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget_App_ReactJS/static/js/2.ce226546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8f5b7c40b5f5e30e71",
+    "url": "/Budget_App_ReactJS/static/js/main.ea37d72a.chunk.js"
+  },
+  {
+    "revision": "85801d315485d31fd962",
+    "url": "/Budget_App_ReactJS/static/js/runtime-main.098f19e4.js"
+  }
+]);
